4426b4f6e4ada9e5e4b1bed31b62d119d96485ea
Urs Gerber <ug.gerber@gmail.com>
2015-09-24 16:23:42 +0200

    Fix user register bug, add register link to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_login
+  before_action :require_login, only: :index
 
   # GET /users
   def index
