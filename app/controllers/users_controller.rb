723ac1f90927ea3c7000a997b63dae2653a6533f
Gian-Luca Mateo <gianluca.mateo@gmail.com>
2015-09-24 16:05:52 +0200

    basic user login working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_login
 
   # GET /users
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
+  #resources :user_sessions
   resources :users
+
+  get 'login' => 'user_sessions#new', :as => :get_login
+  post 'login' => 'user_sessions#create', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
   resources :auctions, shallow: true do
     resources :bids
   end
