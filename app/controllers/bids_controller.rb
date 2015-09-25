5b6250d6fd7609f946d539c01507af0da25e4519
Mansour Hamidi <mansourhamidi@hotmail.com>
2015-09-25 18:47:28 +0200

    auction timing issues

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -16,15 +16,22 @@ class BidsController < ApplicationController
   def create
     @bid = @auction.bids.create(bid_params)
 
-    if @bid.save
-      redirect_to @auction, notice: 'Bid was successfully created.'
+    if bid_is_high_enough? and bid_is_within_time?
+      @bid.save
+      @auction.update_price(@bid.amount)
+      @auction.update_time
+      flash[:success] = 'Bid successfully created'
+    elsif !bid_is_within_time?
+      @bid.destroy
+      flash[:error] = 'Auction time is over'
     else
-      render :new
+      @bid.destroy
+      flash[:error] = 'Bid is too low'
     end
+    redirect_to @auction
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_bid
       @bid = Bid.find(params[:id])
     end
@@ -33,8 +40,20 @@ class BidsController < ApplicationController
       @auction = Auction.find(params[:auction_id])
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def bid_params
-      params.require(:bid).permit(:amount)
+    def bid_is_within_time?
+      @auction.end_time.to_i > Time.now.to_i + 7200
     end
+
+    def bid_is_high_enough?
+      if @auction.bids.empty?
+        @bid.amount >= @auction.price
+      else
+        @bid.amount > @auction.price
+      end
+    end
+
+  def bid_params
+    params.require(:bid).permit(:amount)
+  end
+
 end
--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,5 +1,22 @@
 class Auction < ActiveRecord::Base
   validates_presence_of :brand, :model, :price
+  validates :price, :numericality => { :greater_than_or_equal_to => 1 }
 
   has_many :bids
+
+
+  def update_price max_bid
+    self.price = max_bid
+    self.save
+  end
+
+  def update_time
+    remaining_time = ((self.end_time.to_i - Time.now.to_i)/60 -120)
+
+    if remaining_time < 5
+      self.end_time += 900 # 15min
+      self.save
+    end
+  end
+
 end
