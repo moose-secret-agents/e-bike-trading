4d56812011f176a8ea5517d4cb75edaac94ff27b
Urs Gerber <ug.gerber@gmail.com>
2015-09-28 10:27:21 +0200

    Only end non-ended auctions

--- lib/tasks/task_helpers.rb
+++ lib/tasks/task_helpers.rb
@@ -1,11 +1,11 @@
 module TaskHelpers
 
   def self.end_overdue_auctions
-    count = Auction.overdue.count
+    count = Auction.overdue.select { |a| !a.ended? }.count
     Rails.logger.debug "Ending #{count} overdue auctions now"
 
     Auction.overdue.each do |auction|
-      auction.end
+      auction.end unless auction.ended?
     end
   end
 
